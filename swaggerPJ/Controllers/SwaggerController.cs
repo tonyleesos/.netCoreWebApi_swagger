035d572799079f9c65b4ae1eccf39118ed98dd6f
leeyucheng <leeyucheng@cht.com.tw>
2023-01-05 17:07:46 +0800

    [leeyucheng] 新增swaggerController專案

---
+++ swaggerPJ/Controllers/SwaggerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using swaggerPJ.Models;
+
+namespace swaggerPJ.Controllers
+{   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SwaggerController : ControllerBase
+    {
+        private readonly SwaggerContext _swaggerContext;
+        public SwaggerController(SwaggerContext swaggerContext)
+        {
+            _swaggerContext = swaggerContext;
+        }
+        public void JsonContentGetContent()
+        {
+           
+        }
+    }
+}
